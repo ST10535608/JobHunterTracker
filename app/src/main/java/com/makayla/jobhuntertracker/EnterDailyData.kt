942c4d0ebcabaafd231b77e417a4c2e32a4459ac
ST10535608 <ST10535608@myemeris.edu.za>
2026-06-09 15:43:54 +0200

    Second Screen and first two buttons logic

---
+++ app/src/main/java/com/makayla/jobhuntertracker/EnterDailyData.kt
@@ -0,0 +1,15 @@
+package com.makayla.jobhuntertracker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EnterDailyData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_enter_daily_data)
+    }
+}
--- app/src/main/java/com/makayla/jobhuntertracker/MainActivity.kt
+++ app/src/main/java/com/makayla/jobhuntertracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.makayla.jobhuntertracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnStartActivity = findViewById<Button>(R.id.btnStart)
+        val btnExitApp = findViewById<Button>(R.id.btnExit)
+
+        // button which closes app
+        btnExitApp.setOnClickListener {
+            finishAffinity()
+        }
+
+        // button which allows user to move to next screen
+        btnStartActivity.setOnClickListener {
+            val intent = Intent(this, EnterDailyData::class.java)
+            startActivity(intent)
+        }
     }
 }
